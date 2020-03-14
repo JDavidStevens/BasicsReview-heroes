b50fd5402caa297ea999be87fc0a84c48dd8c806
David Stevens <james.stevens@healthcatalyst.com>
2020-03-14 12:28:54 -0600

    intial commit

---
+++ src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _heroService: HeroService,
+    private _location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  public getHero(): void {
+    const id = +this._route.snapshot.paramMap.get('id');
+    this._heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  public goBack(): void {
+    this._location.back();
+  }
+}
